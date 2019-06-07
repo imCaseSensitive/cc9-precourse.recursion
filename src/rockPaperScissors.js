cab2fe8f9e50b20ca5806b54fefe24d506588015
Matt Weidner <mweidner44@gmail.com>
2019-06-07 21:09:14 +0900

    Completed Rock, paper, scissors function

--- src/rockPaperScissors.js
+++ src/rockPaperScissors.js
@@ -17,6 +17,27 @@
  *   rockPaperScissors(4); // => [['rock', 'rock', 'rock', 'rock'], etc...]
  */
 
-const rockPaperScissors = () => {
-  // Your code here
+const rockPaperScissors = (number) => {
+  let rounds = number;
+  let sleights = ["rock", "paper", "scissors"];
+  let result = [];
+
+  if (typeof number === "undefined") {
+    rounds = 3;
+  };
+
+  function recurse(remainingRounds, played) {
+    if (remainingRounds === 0) {
+      result.push(played);
+      return 
+    } else {
+      for (let i = 0; i < sleights.length; i++) {
+        let currentSleight = sleights[i];
+        recurse(remainingRounds - 1, played.concat(currentSleight));
+      }
+    }
+    return result;
+
+  }
+  return recurse(rounds, []);
 };
